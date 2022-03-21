109bec01d2f510d73d8e893a58382c35546e732a
VukasinVulovic <vukasinvulovic.radninalog@gmail.com>
2022-03-21 09:50:00 +0100

    added nav

--- src/DestSelector/index.js
+++ src/DestSelector/index.js
@@ -8,8 +8,54 @@ function DestSelector() {
             <h1 className='label'> Index </h1>
 
             <nav className='nav-bar'>
-                <a className='nav-item'> Aneks G </a>
-                <a className='nav-item'> Aneks M </a>
+                <a className='nav-item'> 
+                    <label className="label"> Aneks G </label>
+                    
+                    <div className="items">
+                        <a className="item"> G1 </a>
+                        <a className="item"> G2 </a>
+                        <a className="item"> G3 </a>
+                        <a className="item"> G4 </a>
+                    </div>
+                </a>
+
+                <a className='nav-item'> 
+                    <label className="label"> Aneks M </label>
+                    
+                    <div className="items">
+                        <a className="item"> M1 </a>
+                        <a className="item"> M2 </a>
+                        <a className="item"> M3 </a>
+                        <a className="item"> M4 </a>
+                    </div>
+                </a>
+
+                <a className='nav-item'> 
+                    <label className="label"> Hrana i Piće </label>
+                    
+                    <div className="items">
+                        <a className="item"> Menza </a>
+                        <a className="item"> Kafić </a>
+                    </div>
+                </a>
+
+                <a className='nav-item'> 
+                    <label className="label"> Dešavanja </label>
+                    
+                    <div className="items">
+                        <a className="item"> Event Hall </a>
+                    </div>
+                </a>
+
+                <a className='nav-item'> 
+                    <label className="label"> Kancelarije </label>
+                    
+                    <div className="items">
+                        <a className="item"> Zbornica </a>
+                        <a className="item"> Direktor </a>
+                        <a className="item"> Legalna sl. </a>
+                    </div>
+                </a>
             </nav>
         </div>
     );
